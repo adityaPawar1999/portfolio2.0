0b9f37e18707e40d44c3c1f3325b3075e1041ed1
aditya pawar <113058497+adityaPawar1999@users.noreply.github.com>
2023-11-25 13:27:21 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,15 +2,13 @@
 import FooterStyle from "./FooterStyle"
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import { NavLink } from "react-router-dom";
+import InstagramIcon from '@mui/icons-material/Instagram';
 
+import { NavLink } from "react-router-dom";
 
 import PlaceIcon from '@mui/icons-material/Place';
 import EmailIcon from '@mui/icons-material/Email';
 import CallIcon from '@mui/icons-material/Call';
-
-
-
 
 const Footer = () => {
     return (<>
@@ -26,8 +24,8 @@ const Footer = () => {
                             <a href='https://www.linkedin.com/in/aditya-pawar-857247216/' target='_blank' rel='noopener noreferrer'>
                             <span className="linkedLink link"><LinkedInIcon/></span>
                             </a>
-                            <a href="tel:+918888359809 " target='_blank' rel='noopener noreferrer'>
-                            <span className="callLink link"><CallIcon/></span>
+                            <a href="https://instagram.com/adityapawa.r/?igshid=NTc4MTIwNjQ2YQ%3D%3D " target='_blank' rel='noopener noreferrer'>
+                            <span className="callLink link"><InstagramIcon/></span>
                             </a>
                         </div>
                     </div>
