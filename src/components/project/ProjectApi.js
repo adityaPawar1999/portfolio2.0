4b88302bf78a673228858c8fe093e2a0420b0cd0
aditya pawar <113058497+adityaPawar1999@users.noreply.github.com>
2023-11-26 15:00:42 +0530

    Update ProjectApi.js
    link broken

--- src/components/project/ProjectApi.js
+++ src/components/project/ProjectApi.js
@@ -5,21 +5,18 @@ const data=[
         img:"https://images.pexels.com/photos/3584994/pexels-photo-3584994.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         descriptiom:"This project showcases React, Styled Components, NodeJs, and MongoDB database delivering a seamless blend of modern design, functionality, and responsiveness.",
         gitLink:'https://github.com/adityaPawar1999/RestFulApi',
-        siteLink:'Error',
     },{
         id:2,
         title:"WEATHER WEBAPP",
         img:"https://images.unsplash.com/photo-1614480633894-f3b7f4bb0e76?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         descriptiom:"Powerful weather application accessible directly from your web browser, delivering accurate and detailed weather forecasts right to your desktop.",
         gitLink:'https://github.com/adityaPawar1999/Weather-App',
-        siteLink:"error"
     },{
         id:3,
         title:"UPDATEDAILY WEBSITE UI",
         img:"https://images.unsplash.com/photo-1573867639040-6dd25fa5f597?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         descriptiom:"Explore bloghub , a React project where I've crafted a vibrant blog website using styled components. Enjoy a visually stunning and user-friendly....",
         gitLink:"https://github.com/adityaPawar1999/bloghub",
-        siteLink:" https://peaceful-peony-86715b.netlify.app/",
 
     },{
         id:4,
@@ -27,8 +24,6 @@ const data=[
         img:"https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         descriptiom:"A Full MERN (MongoDB, Express.js, React, Node.js) Todo application represents a powerful and versatile tool for task management.",
         gitLink:"https://github.com/adityaPawar1999/TODOAPP",
-        siteLink:"Error",
-
     },
 ];
 
