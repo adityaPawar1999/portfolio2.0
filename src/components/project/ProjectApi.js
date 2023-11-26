331f22ec10ffa0719ccab7074b5d4d09a272f065
aditya pawar <113058497+adityaPawar1999@users.noreply.github.com>
2023-11-27 02:53:28 +0530

    ProjectApi.js

--- src/components/project/ProjectApi.js
+++ src/components/project/ProjectApi.js
@@ -4,7 +4,7 @@ const data=[
         title:"BLOGSPOT(Mern)",
         img:"https://images.pexels.com/photos/3584994/pexels-photo-3584994.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         descriptiom:"This project showcases React, Styled Components, NodeJs, and MongoDB database delivering a seamless blend of modern design, functionality, and responsiveness.",
-        gitLink:'https://github.com/adityaPawar1999/RestFulApi',
+        gitLink:'https://github.com/adityaPawar1999/blogspot',
         siteLink:'Error',
     },{
         id:2,
